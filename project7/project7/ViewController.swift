512f27c4fb67725e4fd4245854e8ce4c21cf9fd0
Artem <artchzhen@gmail.com>
2023-04-11 20:36:34 +0900

    itial comit devolopment

---
+++ project7/project7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project7
+//
+//  Created by Артем Чжен on 11/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
